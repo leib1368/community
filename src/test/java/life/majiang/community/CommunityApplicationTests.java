44c900aa1774e918159e14b98794a593a1dd57ca
leib1368 <leib1368@gmail.com>
2020-11-02 10:33:30 +0800

    删除test

--- src/test/java/life/majiang/community/CommunityApplicationTests.java
+++ src/test/java/life/majiang/community/CommunityApplicationTests.java
@@ -1,16 +1,27 @@
 package life.majiang.community;
 
+import life.majiang.community.mapper.QuestionExtMapper;
+import life.majiang.community.mapper.QuestionMapper;
+import life.majiang.community.model.Question;
+import life.majiang.community.model.QuestionExample;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CommunityApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	QuestionExtMapper questionExtMapper;
+
+	@Autowired
+	QuestionMapper questionMapper;
+
 
 }
