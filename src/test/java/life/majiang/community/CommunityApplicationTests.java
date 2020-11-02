113f82d5122b8fc29fc416d3548f92149a4fe78c
leib1368 <leib1368@gmail.com>
2020-11-02 10:52:13 +0800

    test

--- src/test/java/life/majiang/community/CommunityApplicationTests.java
+++ src/test/java/life/majiang/community/CommunityApplicationTests.java
@@ -23,5 +23,29 @@ public class CommunityApplicationTests {
 	@Autowired
 	QuestionMapper questionMapper;
 
+	@Test
+	@Rollback()
+	@Transactional(propagation= Propagation.REQUIRED )
+	public void update1() {
+		//通过主键查询问题
+		Question record = questionMapper.selectByPrimaryKey((long) 3);
+		//设置浏览数为11
+		record.setViewCount(111);
+		//执行更新
+		questionMapper.updateByPrimaryKey(record);
+		update2();
+	}
+
+	@Test
+	//@Rollback()
+	@Transactional(propagation= Propagation.REQUIRED )
+	public void update2() {
+		//通过主键查询问题
+		Question record = questionMapper.selectByPrimaryKey((long) 3);
+		//设置浏览数为11
+		record.setViewCount(222);
+		//执行更新
+		questionMapper.updateByPrimaryKey(record);
+	}
 
 }
